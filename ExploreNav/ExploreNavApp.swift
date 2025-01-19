616ff509cb17c4f8cfd428f750bf90bcfdf4d06d
Gustavo Araujo Santos <gustavoaraujo600@hotmail.com>
2025-01-19 11:59:42 -0300

    Initial Commit

---
+++ ExploreNav/ExploreNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreNavApp.swift
+//  ExploreNav
+//
+//  Created by Gustavo Araujo Santos on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ExploreNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
